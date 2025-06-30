220b1f4ee98bd75b9a06c62ff1f8cf00adef11c9
Dylan Tettarasar <dtettarasar@gmail.com>
2025-06-30 15:37:57 +0000

    created img_protection.py

---
+++ img_protection.py
@@ -0,0 +1,14 @@
+class Img_Protection:
+
+    def __init__(self, img_path):
+        
+        self.img_path = img_path
+
+    @property
+    def img_path(self):
+        return self._img_path
+    
+
+    @img_path.setter
+    def img_path(self, value):
+        self._img_path = value
