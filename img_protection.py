9ab283b1ef71459e5cc46dae4d594b1dee51b670
Dylan Tettarasar <dtettarasar@gmail.com>
2025-07-01 09:42:33 +0000

    img protection class: create the debug mode attribute

--- img_protection.py
+++ img_protection.py
@@ -1,8 +1,9 @@
 class Img_Protection:
 
-    def __init__(self, img_path):
+    def __init__(self, img_path, debug_mode=False):
         
         self.img_path = img_path
+        self.debug_mode = debug_mode
 
     @property
     def img_path(self):
@@ -12,3 +13,11 @@ class Img_Protection:
     @img_path.setter
     def img_path(self, value):
         self._img_path = value
+
+    @property
+    def debug_mode(self):
+        return self._debug_mode
+    
+    @debug_mode.setter
+    def debug_mode(self, value):
+        self._debug_mode = value
--- test_img_protection.py
+++ test_img_protection.py
@@ -5,3 +5,8 @@ def test_init_class():
 
     img = Img_Protection('test_img.jpg')
     assert img.img_path == 'test_img.jpg'
+    assert img.debug_mode == False
+
+    img_debug = Img_Protection('test_img_debug.jpg', True)
+    assert img_debug.img_path == 'test_img_debug.jpg'
+    assert img_debug.debug_mode == True
