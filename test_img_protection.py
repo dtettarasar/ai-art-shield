24288d06fa92c0543bee78126b0f50c0dacd3a0f
Dylan Tettarasar <dtettarasar@gmail.com>
2025-07-01 11:27:03 +0000

    add test for load file method

--- test_img_protection.py
+++ test_img_protection.py
@@ -3,6 +3,7 @@ import pytest
 import PIL
 from PIL import Image, UnidentifiedImageError
 from PIL.JpegImagePlugin import JpegImageFile
+from PIL.WebPImagePlugin import WebPImageFile
 
 from img_data import Img_Data
 
@@ -15,9 +16,11 @@ def test_init_class():
 def test_load_image_file():
 
     img_cs50 = Img_Data('test_files/cs50.jpg')
+    img_cookie = Img_Data("test_files/cookie_monster.webp")
 
     # Test la classe abstraite
     assert isinstance(img_cs50.img_file, Image.Image)
+    assert isinstance(img_cookie.img_file, Image.Image)
 
     # Test la sous classe de la variable
     assert type(img_cs50.img_file) == JpegImageFile
@@ -26,6 +29,15 @@ def test_load_image_file():
     assert img_cs50.img_file.width == 2048
     assert img_cs50.img_file.height == 1366
     assert img_cs50.img_file.format == "JPEG"
+
+    assert type(img_cookie.img_file) == WebPImageFile
+
+    assert img_cookie.img_file.mode == "RGB"
+    assert img_cookie.img_file.width == 1348
+    assert img_cookie.img_file.height == 1600
+    assert img_cookie.img_file.format == "WEBP"
+
+
 
 def test_load_image_file_errors():
 
