b053b06a76471123e45e60ad4bee96bfcc1f04ad
Dylan Tettarasar <dtettarasar@gmail.com>
2025-07-02 10:10:00 +0000

    in test load image file: replaced locale instance by fixtures

--- test_img_protection.py
+++ test_img_protection.py
@@ -28,29 +28,26 @@ def test_init_class(img_cs50_instance, img_cookie_instance):
 
 # Test for load_file method------------------------------
 
-def test_load_image_file():
-
-    img_cs50 = Img_Data('test_files/cs50.jpg')
-    img_cookie = Img_Data("test_files/cookie_monster.webp")
+def test_load_image_file(img_cs50_instance, img_cookie_instance):
 
     # Test la classe abstraite
-    assert isinstance(img_cs50.img_file, Image.Image)
-    assert isinstance(img_cookie.img_file, Image.Image)
+    assert isinstance(img_cs50_instance.img_file, Image.Image)
+    assert isinstance(img_cookie_instance.img_file, Image.Image)
 
     # Test la sous classe de la variable
-    assert type(img_cs50.img_file) == JpegImageFile
+    assert type(img_cs50_instance.img_file) == JpegImageFile
 
-    assert img_cs50.img_file.mode == "RGB"
-    assert img_cs50.img_file.width == 2048
-    assert img_cs50.img_file.height == 1366
-    assert img_cs50.img_file.format == "JPEG"
+    assert img_cs50_instance.img_file.mode == "RGB"
+    assert img_cs50_instance.img_file.width == 2048
+    assert img_cs50_instance.img_file.height == 1366
+    assert img_cs50_instance.img_file.format == "JPEG"
 
-    assert type(img_cookie.img_file) == WebPImageFile
+    assert type(img_cookie_instance.img_file) == WebPImageFile
 
-    assert img_cookie.img_file.mode == "RGB"
-    assert img_cookie.img_file.width == 1348
-    assert img_cookie.img_file.height == 1600
-    assert img_cookie.img_file.format == "WEBP"
+    assert img_cookie_instance.img_file.mode == "RGB"
+    assert img_cookie_instance.img_file.width == 1348
+    assert img_cookie_instance.img_file.height == 1600
+    assert img_cookie_instance.img_file.format == "WEBP"
 
 def test_load_image_file_errors():
 
