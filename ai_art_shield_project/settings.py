08a2bad5cedd107a4b166d39a921375b5041c5dd
Dylan Tettarasar <dtettarasar@gmail.com>
2025-07-13 10:43:56 +0000

    building view for image upload process

--- ai_art_shield_project/settings.py
+++ ai_art_shield_project/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Settings for user-uploaded media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media' # Définit le chemin absolu vers le dossier 'media' à la racine du projet
+
--- protection_app/views.py
+++ protection_app/views.py
@@ -1,8 +1,13 @@
 # protection_app/views.py
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
 from .forms import ImageUploadForm
+
+import os
 
 def upload_image_view(request):
 
@@ -27,6 +32,21 @@ def upload_image_view(request):
 
             print("protection_strength: ")
             print(protection_strength)
+
+            # A checker avant : MEDIA_ROOT doit être configuré dans settings.py
+            # Sinon, cette partie échouera.
+            fs = FileSystemStorage()
+
+            # Nommer le fichier de manière unique pour éviter les conflits
+            uploaded_filename = fs.save(uploaded_image.name, uploaded_image)
+            uploaded_file_path = os.path.join(settings.MEDIA_ROOT, uploaded_filename)
+
+            print("uploaded_filename: ")
+            print(uploaded_filename)
+
+            print("uploaded_file_path: ")
+            print(uploaded_file_path)
+
 
         else:
             # Si le formulaire n'est pas valide (ex: mauvais type de fichier, force hors limites)
