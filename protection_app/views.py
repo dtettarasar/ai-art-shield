ffd9a33393cd368b1eafe6cda7bd0e1aac1efd0c
Dylan Tettarasar <dtettarasar@gmail.com>
2025-07-13 10:14:31 +0000

    building view for upload image page

--- protection_app/views.py
+++ protection_app/views.py
@@ -10,4 +10,27 @@ def upload_image_view(request):
 
         form = ImageUploadForm()
 
+    elif request.method == 'POST':
+
+        print('post request from image upload form')
+
+        form = ImageUploadForm(request.POST, request.FILES)
+
+        if form.is_valid():
+
+            # Récupérer l'image et la force de protection
+            uploaded_image = form.cleaned_data['image']
+            protection_strength = form.cleaned_data['strength']
+
+            print("uploaded_image: ")
+            print(uploaded_image)
+
+            print("protection_strength: ")
+            print(protection_strength)
+
+        else:
+            # Si le formulaire n'est pas valide (ex: mauvais type de fichier, force hors limites)
+            # Le template affichera automatiquement les erreurs via {{ form.errors }} ou {{ field.errors }}
+            pass
+
     return render(request, 'protection_app/upload_image.html', {'form': form})
